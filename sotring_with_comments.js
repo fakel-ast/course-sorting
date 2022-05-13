43120389eecb9f5fcf28c044d53a682b6395c178
fakel_ast <lvov-styopa@ya.ru>
2022-05-13 21:35:55 +0300

    Added filtering code

---
+++ sotring_with_comments.js
@@ -0,0 +1,77 @@
+// Список курсов
+let courses = [
+    {name: "Courses in England", prices: [0, 100]},
+    {name: "Courses in Germany", prices: [500, null]},
+    {name: "Courses in Italy", prices: [100, 200]},
+    {name: "Courses in Russia", prices: [null, 400]},
+    {name: "Lol 1", prices: [50, 75]},
+    {name: "Lol 2", prices: [50, 700]},
+    {name: "Lol 3", prices: [50, null]},
+    {name: "Lol 4", prices: [null, 50]},
+    {name: "Courses in China", prices: [50, 250]},
+    {name: "Courses in USA", prices: [200, null]},
+    {name: "Courses in Kazakhstan", prices: [56, 324]},
+    {name: "Courses in France", prices: [null, null]},
+];
+
+console.log(courses.map(course => course.prices))
+// Варианты цен (фильтры), которые ищет пользователь
+let requiredRange1 = [null, 200];
+let requiredRange2 = [100, 350];
+let requiredRange3 = [200, null];
+
+
+const isGreater = (priceOne, priceTwo) => {
+    if (priceOne === null || priceTwo === null) {
+        return true;
+    } else if (priceOne >= priceTwo) {
+        return true;
+    }
+    return false;
+}
+
+const sortingCourses = courses => {
+    return courses.sort((a, b) => (a.prices[0] || 0) - (b.prices[0]) || 0)
+}
+
+const courseFiltering = (inputData) => {
+    const {range, courses} = inputData;
+    const [rangeStart, rangeEnd] = range
+    const filteredCourses = courses.filter(course => {
+        const [priceStart, priceEnd] = course.prices;
+        if (rangeStart === null) {
+            // Если первое число null (0), берем те, у которых конец диапазона (200) > начало цены
+            return isGreater(rangeEnd, priceStart)
+        } else if (rangeEnd === null) {
+            // Если второе число null (Inf), берем те, у которых конец цены (200, 300, 400, 0, 75) > начало диапазона
+            return isGreater(priceEnd, rangeStart)
+        }
+        // Если начало диапазона > начало цены ИЛИ конец диапазона > начало цены И начало диапазона < конец цены.
+        // Ну из зачем нам это? Давайте разберем при фильтре [100 - 350]
+        // Допустим у нас есть примеры: [0 - 100], [50 - 75], [50 - Inf], [56 - 324], [0 - 50], [400 - 500]
+        // 100 >= 0 || 350 >= 0  ДА [0 - 100]
+        // 100 >= 50 || 350 >= 50 ДА [50 - 75]
+        // 100 >= 50 || 350 >= 50 ДА [50 - Inf]
+        // 100 >= 56 || 350 >= 56 ДА [56 - 324]
+        // 100 >= 0 || 350 >= 0 ДА [0 - 50]
+        // 100 >= 400 || 350 >= 400 НЕТ [400 -500 ]
+        // При первом или у нас останутся все кроме [400 - 500]
+        // НО 50 - 75 не входит в наш диапазон.
+        // Поэтому делаем проверку чтобы конец цены был больше начала фильтра.
+        // То есть
+        // 100 >= 100 ДА  [0 - 100]
+        // 75 >= 100 НЕТ [50 - 75]
+        // Inf >= 100 ДА [50 - Inf]
+        // 324 >= 100 ДА [56 - 324]
+        // 50 >= 100 НЕТ [0 - 50]
+        // Получается, что мы оставляем [0 - 100], [50 - Inf], [56 - 324]
+        return (isGreater(rangeStart, priceStart) || isGreater(rangeEnd, priceStart)) && isGreater(priceEnd, rangeStart)
+    })
+    return sortingCourses(filteredCourses)
+}
+
+const result1 = courseFiltering({range: requiredRange1, courses});
+const result2 = courseFiltering({range: requiredRange2, courses});
+const result3 = courseFiltering({range: requiredRange3, courses});
+
+console.log(result3)
